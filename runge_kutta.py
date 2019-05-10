e3d1b23dfec086dadcc1a2acb3c214a406f041ca
Michele Stofella <michele.stofella@studio.unibo.it>
2019-05-10 19:06:22 +0200

    Test by changing integration step (part 2);\n TO DO: control the error!!

--- runge_kutta.py
+++ runge_kutta.py
@@ -35,15 +35,15 @@ y0=10
 
 x = np.linspace(0,dt*Nstep,Nstep)    
 y = RK4(f, y0)    
-plt.scatter(x,y)
+plt.plot(x,y)
 
 # %%
 # We test the algorithm in the simple case dy/dt=-y
 # where the solution is expected to be an exponential decrease
-def exact_exp(y0,x):
+def exact_exp(x,y0):
     return y0*np.exp(-x)
 # We test it by changing the integration step delta_t
-delta_t = [1., 0.5, 0.1, 0.05, 0.01]
+delta_t = [0.01, 0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09, 0.1]
 total_time = 5
 
 for dt in delta_t:
@@ -51,7 +51,37 @@ for dt in delta_t:
     y = RK4(f,y0,dt,round(total_time/dt))
     plt.plot(x,y,label="dt="+str(dt))
 
-plt.plot(x,exact_exp(y0,x),c='black',label="Exact")
+plt.plot(np.linspace(0,5,1000),exact_exp(np.linspace(0,5,1000),y0),c='black',label="Exact")
 plt.legend()
+
+# %%
+# Second part of the test: we evaluate the distance between the 
+# approximated curves and the exact curve
+
+# Error between the dt=1.00 curve and the exact one
+x = np.linspace(0, total_time, round(total_time/delta_t[0]))
+y = RK4(f,y0,delta_t[0],round(total_time/delta_t[0]))
+exact_y = exact_exp(x,y0)
+
+err = np.zeros(round(total_time/delta_t[0]))
+for i in range(0,round(total_time/delta_t[0])):
+    err[i] = np.abs(y[i]-exact_y[i])
+plt.scatter(x,err)
+    
+# %%
+
+# I want a plot Error vs Deltat
+mean_err = np.zeros(len(delta_t))
+for i in range(0,len(delta_t)):
+    x = np.linspace(0, total_time, round(total_time/delta_t[i]))
+    y = RK4(f,y0,delta_t[i],round(total_time/delta_t[i]))
+    exact_y = exact_exp(x,y0)
+    mean_err[i] = np.abs(y[round(total_time/delta_t[i])-1]-exact_y[round(total_time/delta_t[i])-1])
+plt.scatter(delta_t,mean_err, c='black')
+plt.xlim(0,0.1); plt.ylim(0,0.1)
+plt.xlabel("dt", fontsize=18); plt.ylabel("Mean Error", fontsize=18)
+
+xx = np.linspace(0,0.1,1000)
+plt.plot(xx, xx**1.2)    
 
 # %%
