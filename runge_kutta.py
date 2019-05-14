3977f03fb3fbadcbabdc80bd3b21000cf4cf3b16
Michele Stofella <michele.stofella@studio.unibo.it>
2019-05-15 00:24:57 +0200

    Added second version; introduced data frame for error analysis; added second sample function

--- runge_kutta.py
+++ runge_kutta.py
@@ -89,17 +89,57 @@ plt.plot(xx, poly(xx), 'r--')
 # %%
 # =============================================================================
 # SECOND VERIONS OF RUNGE-KUTTA ALGORITHM
+# This second version uses only the function f
+# and not its derivative. 
+"""
+dy/dt = f(t,y)
+"""
+# source: https://rosettacode.org/wiki/Runge-Kutta_method
 # =============================================================================
 
-# http://www2.hawaii.edu/~jmcfatri/math407/RungeKuttaTest.html
+# we try with a sample function of which we know the analitical solution
+def f (t,y):
+    return t*np.sqrt(y)
+
+def exact_solution (t):
+    return (t**2 + 4)**2/16
+
+dt = 0.1
+total_time = 10
+Nstep = round(total_time/dt)
+t0 = 0
+y0 = 1
+
+dy1 = np.zeros(Nstep); dy2 = np.zeros(Nstep); dy3 = np.zeros(Nstep)
+dy4 = np.zeros(Nstep); y = np.zeros(Nstep); t = np.zeros(Nstep)
+t[0] = t0; y[0] = y0
+for i in range(0,Nstep-1):
+    dy1[i] = dt*f(t[i], y[i])
+    dy2[i] = dt*f(t[i]+0.5*dt, y[i]+0.5*dy1[i])
+    dy3[i] = dt*f(t[i]+0.5*dt, y[i]+0.5*dy2[i])
+    dy4[i] = dt*f(t[i]+dt, y[i]+dy3[i])
+
+    y[i+1] = y[i] + (dy1[i] + 2*dy2[i] + 2*dy3[i] + dy4[i])/6
+    t[i+1] = t[i] + dt
+error = np.abs(y - exact_solution(t))
+
+x = np.linspace(0,10,1000)
+plt.plot(t,y)
+plt.plot(x,exact_solution(x))
+# %%
+import pandas as pd
+d = {'Numerical' : y, 
+     'Analytical' : exact_solution(t),
+     'Error' : error}
+res = pd.DataFrame(d)
+res.head(10)
 
 
+coef = np.polyfit(t, error, 5)
+poly = np.poly1d(coef)
+xx = np.linspace(0,10,1000)
 
-
-
-
-
-
-
+plt.plot(t,error)
+plt.plot(xx, poly(xx), 'r--') 
 
 # %%
