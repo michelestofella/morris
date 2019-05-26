e059418d58ab94b3ed06dc21aefc82f7a092ae5c
Michele Stofella <michele.stofella@studio.unibo.it>
2019-05-27 01:10:56 +0200

    First version of Runge Kutta 4 algorithm with tests

---
+++ rk4.py
@@ -0,0 +1,57 @@
+# =============================================================================
+# RUNGE-KUTTA ALGORITHM
+#   
+#     dy/dt = f(t,y)
+#
+# source: https://rosettacode.org/wiki/Runge-Kutta_method
+# =============================================================================
+
+import numpy as np
+
+def RK4(f, dt, y0, t0, Nstep):
+    y = np.zeros(Nstep+1); t = np.zeros(Nstep+1)
+    t[0] = t0; y[0] = y0
+    for i in range(0,Nstep):
+        dy1 = dt*f(t, y)
+        dy2 = dt*f(t+0.5*dt, y+0.5*dy1)
+        dy3 = dt*f(t+0.5*dt, y+0.5*dy2)
+        dy4 = dt*f(t+dt,     y+dy3)
+        
+        y[i+1] = y[i] + (dy1[i] + 2*dy2[i] + 2*dy3[i] + dy4[i])/6
+        t[i+1] = t[i] + dt
+    return t,y 
+
+# FIRST TEST
+# We apply the algorithm to a specific function
+# we move forward and backward
+# we expect the final value to be equal to the initial one
+# within a tolerance tol
+def test_one():
+    tol = 1e-5
+    def f(t,y):
+        return t*np.sqrt(y)
+    dt = 0.1; y0 = 1.0; t0 = 0.0; Nstep = 100
+    t_for, y_for = RK4(f, dt, y0, t0, Nstep)
+    y_last = y_for[-1]
+    t_back,y_back= RK4(f,-dt, y_last, t0+dt*Nstep, Nstep)
+    assert (y_back[-1] < y0+tol) & (y_back[-1] > y0-tol) 
+    
+# SECOND TEST
+# We apply the algorithm to a specific function
+# we move from a to b, then from b to c
+# and directly from a to c
+# we expect the final values to be equal
+# within a tolerance tol
+def test_two():
+    tol = 1e-5
+    def f(t,y):
+        return t*np.sqrt(y)
+    dt = 0.1; y0 = 1.0; t0 = 0.0
+    N_ab = 100; N_bc = 50 
+    t_ab, y_ab = RK4(f, dt, y0, t0, N_ab)
+    t_bc, y_bc = RK4(f, dt, y_ab[-1], t0+N_ab*dt, N_bc)
+    t_ac, y_ac = RK4(f, dt, y0, t0, N_ab+N_bc)
+    assert (y_ac[-1] < y_bc[-1]+tol) & (y_ac[-1] > y_bc[-1]-tol)
+
+
+# %%
--- runge_kutta.py
+++ runge_kutta.py
@@ -29,9 +29,11 @@ def RK4(f, dt, y0, Nstep, t0):
 """ Sample Function 1 """
 def f (t,y):
     return t*np.sqrt(y)
+    # return y
 """ Solution Function 1 """
 def exact_solution (t):
     return (t**2 + 4)**2/16
+    # return np.exp(t)
 
 dt = 0.1
 Nstep = 10
@@ -69,59 +71,11 @@ coef = np.polyfit(dt4, error, 1)
 poly = np.poly1d(coef)
 
 xx = np.linspace(min(dt4),max(dt4),1000)
-""" We visualize the error with respect to log10(dt^4)
-so that we can analyze several order of magnitude for dt """ 
+""" Visualization of the results """ 
 plt.scatter(dt4, error, label='Global Truncation Error', c='black')
 plt.plot(xx, poly(xx), 'r--', label='Error $\propto$ $\Delta$t$^4$') 
 plt.xlabel("$\Delta$t$^4$", fontsize=15)
 plt.ylabel("Error", fontsize=15)
 plt.legend()
-
-# %%
-
-""" Sample Function 2 """
-def f2(y,t):
-    return -t
-""" Solution Function 2 """
-def sol_f2(t,y0):
-    return y0*np.exp(-t)
-
-dt = 0.1
-Nstep = 10
-y0 = 1.0
-t0 = 0.0
-
-y2 = RK4(f2, dt, Nstep=Nstep, y0=y0, t0=t0)
-
-time = np.linspace(t0, t0+Nstep*dt, Nstep)
-x = np.linspace(t0, t0+Nstep*dt, 1000)
-
-plt.plot(time, y2, label='Numerical')
-plt.plot(x, sol_f2(x,y0), label='Analitical')
-plt.legend()
-
-# %%
-
-dt = 0.1; Nstep = 10; y0 = 1.0; t0 = 0.0
-
-time_steps = np.linspace(0.01, 1.0, 20)
-
-error_f2 = np.zeros(len(time_steps))
-for i in range(0,len(time_steps)):
-    y2 = RK4(f2, time_steps[i], Nstep=Nstep, y0=y0, t0=t0)
-    time = np.linspace(t0, Nstep*time_steps[i], Nstep)
-    error_f2[i] = np.abs(y2[-1] - sol_f2(time[-1],y0))
-
-dt4 = np.power(time_steps, 4)
-
-coef_f2 = np.polyfit(dt4, error_f2, 1)
-poly_f2 = np.poly1d(coef_f2)
-
-xx = np.linspace(min(dt4),max(dt4),1000)
-plt.scatter(dt4, error_f2, c='black', label='Global Truncation Error')
-plt.plot(xx, poly_f2(xx), 'r--', label='Error $\propto$ $\Delta$t$^4$')
-plt.xlabel("$\Delta$t$^4$", fontsize=15)
-plt.ylabel("Error", fontsize=15)
-plt.legend() 
 
 # %%
