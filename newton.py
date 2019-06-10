a8dcda56a91ceb90c9aa0dad5b90266e59d1685e
Michele Stofella <michele.stofella@studio.unibo.it>
2019-06-10 23:48:52 +0200

    Newton algorithm (tested)

---
+++ newton.py
@@ -0,0 +1,44 @@
+# =============================================================================
+#
+# NEWTON ALGORITHM
+#   
+# Approximate solution of the equation f(x)=0
+#
+# =============================================================================
+
+def newton(f, Df, x0, eps=1e-16, max_n=20):
+    xn = x0
+    for n in range(0,max_n):
+        f_xn = f(xn)
+        if abs(f_xn) < eps:
+            print('Number of iterations to find the solution: ',n)
+            return xn
+        Df_xn = Df(xn)
+        if Df_xn == 0:
+            print('Zero derivative')
+            return None
+        xn = xn - f_xn/Df_xn
+    print('Maximum number of iterations reached')
+    return None
+ 
+# %%
+    
+def test_one():
+    def f(x):
+        return x**2-4
+    def Df(x):
+        return 2*x
+    x0_1 = 3.0; x0_2 = -1.5
+    assert newton(f,Df,x0_1) == 2.0
+    assert newton(f,Df,x0_2) == -2.0
+
+def test_two():
+    def f(x):
+        return r*x - x**2
+    def Df(x):
+        return r - 2*x
+    r = 3
+    assert newton(f,Df,x0=2.0) == 3
+
+# %%
+    
