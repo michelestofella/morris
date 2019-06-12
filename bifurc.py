d4a237de264c80f1af8ad17c75d974fe5a647c93
Michele Stofella <michele.stofella@studio.unibo.it>
2019-06-12 20:57:44 +0200

    Bifurcation analysis of examples made in classroom

--- bifurc.py
+++ bifurc.py
@@ -4,6 +4,9 @@
 #
 # =============================================================================
 
+import numpy as np
+import matplotlib.pyplot as plt    
+
 """ Newton Algorithm: to find a solution to the equation f(x)=0 """
 def newton(f, Df, x0, eps, max_n=50):
     xn = x0
@@ -22,7 +25,7 @@ def newton(f, Df, x0, eps, max_n=50):
 """ Newton algorithm applied to a set of initial points: from xmin to xmax """
 def find_zeros(f, Df, xmin, xmax, eps, max_n=50):
     zero = []; zeros = []
-    start_points = np.linspace(xmin,xmax,20) 
+    start_points = np.linspace(xmin,xmax,100) 
     for x0 in start_points:
         zero.append(round(newton(f,Df,x0,eps),4))
     for element in zero:
@@ -31,25 +34,78 @@ def find_zeros(f, Df, xmin, xmax, eps, max_n=50):
     return zeros
 
 # %%
-
-import numpy as np
-import matplotlib.pyplot as plt    
-
 """ Monodimensional Case 
-1) Saddle Node Bifurcation: dx/dt = phi(x,r) = -r+x**2 """
+1) Saddle Node Bifurcation: 
+   dx/dt = phi(x,r) = -r+x**2 """
 def f(x):
     return -r+x**2
 def Df(x):
     return 2*x
 
 points = []
-r_values = np.linspace(0.5,5,10)
+r_values = np.linspace(0.5,5,100)
 for r in r_values:
-    zeros = find_zeros(f, Df, xmin=-50, xmax=50, eps=1e-5)
+    zeros = find_zeros(f, Df, xmin=-50, xmax=50, eps=1e-12)
     points.append(zeros) 
 
 for i in range(0,len(r_values)):
     plt.scatter(r_values[i],points[i][0],color='red')
     plt.scatter(r_values[i],points[i][1],color='blue')
     
+# %%
+""" Monodimensional Case
+2) Transcritic Bifurcation
+    dx/dt = phi(x,r) = r*x-x**2 """
+def f(x):
+    return r*x-x**2
+def Df(x):
+    return r-2*x
+
+points = []
+r_values = np.linspace(-5,5,100)
+for r in r_values:
+    zeros = find_zeros(f, Df, xmin=-50, xmax=50, eps=1e-12)
+    points.append(zeros) 
+
+for i in range(0,len(r_values)):
+    plt.scatter(r_values[i],points[i][0],color='red')
+    plt.scatter(r_values[i],points[i][1],color='blue')
+
+# %%    
+""" Monodimensional Case
+3) Fork Bifurcation
+    dx/dt = phi(x,r) = r*x-x**3 """
+def f(x):
+    return r*x-x**3
+def Df(x):
+    return r-3*x**2
+""" Determine equilibrium points for different parameters """
+stable_points = []; unstable_points = []
+r_values = np.linspace(-5,5,100)
+for r in r_values:
+    zeros = find_zeros(f, Df, xmin=-5, xmax=5, eps=1e-12)
+    for i in range(0,len(zeros)):
+        if Df(zeros[i])<=0:
+            stable_points.append([r,zeros[i],'stable'])
+        if Df(zeros[i])>0:
+            unstable_points.append([r,zeros[i],'unstable'])
+""" Visualization of the results """    
+for i in range(0,len(stable_points)):
+    plt.scatter(stable_points[i][0],stable_points[i][1],color='red')
+for j in range(0,len(unstable_points)):
+    plt.scatter(unstable_points[j][0],unstable_points[j][1],color='blue') 
+plt.xlabel('parameter r', fontsize=15)
+plt.ylabel('Equilibrium Point $x^*$', fontsize=15)
+       
+# %%   
+
+import pandas as pd
+
+col = ['par','eq_point','stability']
+df_stable = pd.DataFrame(stable_points,columns=col)
+df_unstable = pd.DataFrame(unstable_points,columns=col)
+df = pd.concat([df_stable,df_unstable])
+
+plt.scatter(df['par'],df['eq_point'])
+
 # %%
