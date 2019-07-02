3a747d8bff82727373a7fc9b8087687ec0bb00c8
Michele Stofella <michele.stofella@studio.unibo.it>
2019-07-02 18:35:38 +0200

    Added description of the function

--- newton/newton.py
+++ newton/newton.py
@@ -9,6 +9,31 @@
 import numpy as np    
 
 def newton(f, Df, x0, eps=1e-16, max_n=50):
+    """ Finds the solution of the equation f(x)=0 
+    
+    Parameters
+    ----------
+    f: function to be put in the equation
+    Df: derivative of the function 
+    x0: initial guess
+    eps: precision of the returned value, i.e. how much the function is close to 0 (default: 1e-16)
+    max_n: maximum number of iterations (default: 50)
+    
+    Returns
+    -------
+    xn: n-th iteration, i.e. the zero of the function
+    
+    Examples
+    --------
+    >>> def f(x):
+    >>>    return x**2-4
+    >>> def Df(x):
+    >>>    return 2*x
+    >>> x0 = 3.0
+    >>> newton(f,Df,x0)
+    Number of iterations to find the solution: 5
+    2.0
+    """
     xn = x0
     for n in range(0,max_n):
         f_xn = f(xn)
--- newton/newton2.py
+++ newton/newton2.py
@@ -11,6 +11,38 @@ import numpy as np
 from numpy.linalg import inv
 
 def newton2(f,Jf,p0,eps=1e-8,max_iter=20):
+    """ Bidimensional Newton algorithm: finds the solutions of the system of equations
+    f1(x,y) = 0
+    f2(x,y) = 0
+    
+    Parameters
+    ----------
+    f: list of the two functions that form the system of equations
+    Jf: jacobian matrix of f
+    p0: initial guess, bidimensional list
+    eps: precision of the returned value, i.e. how close f is close to zero (default: 1e-8)
+    max_iter: maximum number of iterations
+    
+    Returns
+    -------
+    pk: bidimensional array, zero of the bidimensional function f
+    
+    Examples
+    --------
+    >>> p0 = [0.1, 0.7]
+    >>> def f1(x,y):
+    >>>     return 1 - 4*x + 2*x**2 - 2*y**3
+    >>> def f2(x,y):
+    >>>     return -4 + x**4 + 4*y + 4*y**4
+    >>> def f(x,y):
+    >>>     return [f1(x,y),f2(x,y)]
+    >>> def Jf(x,y):
+    >>>     return [[-4+4*x, -6*y**2],
+    >>>             [4*x**3, 4+16*y**3]]
+    >>> newton2(f=f,Jf=Jf,p0=p0)
+    Number of iterations: 3
+    array([0.06177013, 0.72449052])
+    """
     for k in range(0,max_iter):
         f_k = f(p0[0],p0[1])
         Jf_k = Jf(p0[0],p0[1])
@@ -20,7 +52,6 @@ def newton2(f,Jf,p0,eps=1e-8,max_iter=20):
         dist = np.sqrt(np.dot(pk-p0,pk-p0))
         if dist < eps:
             print('Number of iterations: ',k)
-            print('Solution: ',pk)
             return pk
         p0 = pk
     print('Maximum number of iterations reached: ',max_iter)
