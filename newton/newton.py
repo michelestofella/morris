dccb40ea6e7b09c3bd2a8ffc21d88fd9b6b9e955
Michele Stofella <michele.stofella@studio.unibo.it>
2019-07-10 13:19:10 +0200

    Algorithm is now imported; some tests have been added; tests are now in a different file

--- newton/newton.py
+++ newton/newton.py
@@ -6,8 +6,6 @@
 #
 # =============================================================================
 
-import numpy as np    
-
 def newton(f, Df, x0, eps=1e-16, max_n=50):
     """ Finds the solution of the equation f(x)=0 
     
@@ -48,32 +46,4 @@ def newton(f, Df, x0, eps=1e-16, max_n=50):
     print('Maximum number of iterations reached')
     return None
  
-# %%
-    
-def test_one():
-    def f(x):
-        return x**2-4
-    def Df(x):
-        return 2*x
-    x0_1 = 3.0; x0_2 = -1.5
-    assert newton(f,Df,x0_1) == 2.0
-    assert newton(f,Df,x0_2) == -2.0
-
-def test_two():
-    def f(x):
-        return r*x - x**2
-    def Df(x):
-        return r - 2*x
-    r = 3
-    assert newton(f,Df,x0=2.0) == 3
-
-def test_three():
-    tol = 1e-6
-    def h(x):
-        return x**2
-    def Dh(x):
-        return 2*x
-    x0 = 3.
-    assert np.abs(newton(h,Dh,x0))<tol
-
 # %%
--- newton/newton2.py
+++ newton/newton2.py
@@ -57,22 +57,3 @@ def newton2(f,Jf,p0,eps=1e-8,max_iter=20):
     print('Maximum number of iterations reached: ',max_iter)
     
 # %%
-
-def test_one():
-    tol = 1e-6
-    p0 = [0.1, 0.7]
-    def f1(x,y):
-        return 1 - 4*x + 2*x**2 - 2*y**3
-    def f2(x,y):
-        return -4 + x**4 + 4*y + 4*y**4
-    def f(x,y):
-        return [f1(x,y),f2(x,y)]
-    def Jf(x,y):
-        return [[-4+4*x, -6*y**2],
-                [4*x**3, 4+16*y**3]]
-    real_sol = [0.0617701,0.724491]
-    num_sol = newton2(f=f,Jf=Jf,p0=p0)
-    diff = np.abs(num_sol-real_sol)
-    assert np.all(diff < tol)
-
-# %%
