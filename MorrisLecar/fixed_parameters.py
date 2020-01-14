9b81486be09f1d51a6df09b15ad87a3b5314d859
Michele Stofella <michele.stofella@studio.unibo.it>
2020-01-14 11:09:49 +0100

    fixed_parameter.py: parameters are now commented and ordered

--- MorrisLecar/fixed_parameters.py
+++ MorrisLecar/fixed_parameters.py
@@ -12,10 +12,20 @@ sys.path.insert(1, '../newton')
 from newton2 import newton2
 
 """ Fixed Model Parameters """
-g_ca = 20.0;     g_k = 20.;   g_leak = 2.
-E_ca = 50.0;    E_k = -100.; E_leak = -70.
-phi_w = 0.15
-c = 2.
-v_k = -10.; theta_ca = 18.0; theta_k = 13.
+# calcium channels
+g_ca = 20.0         # maximal conductance
+E_ca = 50.0         # Nernst potential
+theta_ca = 18.0     # steady state parameter
+# potassium channels
+g_k = 20.           # maximal conductance
+E_k = -100.         # Nernst potential
+v_k = -10.          # steady state parameter
+theta_k = 13.       # steady state parameter
+# leak current 
+g_leak = 2.         # maximal conductance
+E_leak = -70.       # Nernst potential
+# other parameters
+c = 2.              # membrane conductance
+phi_w = 0.15        # constant term 
 
 # %%
